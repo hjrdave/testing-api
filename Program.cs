6f63c83e30f15bf73f7e3da9223bc75d0eab99c2
David Sanders <dsanders_m633@outlook.com>
2022-08-20 19:32:22 -0400

    postgresql

--- Program.cs
+++ Program.cs
@@ -1,28 +1,44 @@
-using testing_api.Data;
-using Microsoft.EntityFrameworkCore;
+﻿using System.Text.Json.Serialization;
+using WebApi.Helpers;
+using WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// add services to DI container
+{
+    var services = builder.Services;
+    var env = builder.Environment;
+ 
+    services.AddDbContext<DataContext>();
+    services.AddCors();
+    services.AddControllers().AddJsonOptions(x =>
+    {
+        // serialize enums as strings in api responses (e.g. Role)
+        x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<IssueDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+        // ignore omitted parameters on models to enable optional params (e.g. User update)
+        x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+    // configure DI for application services
+    services.AddScoped<IUserService, UserService>();
+}
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// configure HTTP request pipeline
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // global cors policy
+    app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
+    // global error handler
+    app.UseMiddleware<ErrorHandlerMiddleware>();
+
+    app.MapControllers();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+app.Run("http://localhost:4000");
